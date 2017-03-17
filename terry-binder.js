fb974ac711674370724a3b91149e6c4ebfa363c1
Joe Lindsay <j.lindsay@vehikl.com>
2017-03-17 16:05:11 -0400

    update to the bind. and removal of accidental file. LOLZ this is SO GHETTO! Not sure if I should go party of shoot myself

--- terry-binder.js
+++ terry-binder.js
@@ -7,12 +7,27 @@
  *  for simple databinding. Im a fan of angular but it felt like we were going at a simple need
  *  with a tank. We needed something that was low maintenance to get the job done quick and dirty.
  *	*******************************************************************************/
-console.info('This is Terry. What up. Just so you know, I am in the early staged of wripping this script to pieces. So I\'ll be dropping crap to the console LEFT AND RIGHT. If you don\'t like it, go %&!$ a *%#&!');
+var css = "text-shadow: -1px -1px hsl(0,100%,50%), 1px 1px hsl(5.4, 100%, 50%), 3px 2px hsl(10.8, 100%, 50%), 5px 3px hsl(16.2, 100%, 50%), 7px 4px hsl(21.6, 100%, 50%), 9px 5px hsl(27, 100%, 50%), 11px 6px hsl(32.4, 100%, 50%), 13px 7px hsl(37.8, 100%, 50%), 14px 8px hsl(43.2, 100%, 50%), 16px 9px hsl(48.6, 100%, 50%), 18px 10px hsl(54, 100%, 50%), 20px 11px hsl(59.4, 100%, 50%), 22px 12px hsl(64.8, 100%, 50%), 23px 13px hsl(70.2, 100%, 50%), 25px 14px hsl(75.6, 100%, 50%), 27px 15px hsl(81, 100%, 50%), 28px 16px hsl(86.4, 100%, 50%), 30px 17px hsl(91.8, 100%, 50%), 32px 18px hsl(97.2, 100%, 50%), 33px 19px hsl(102.6, 100%, 50%), 35px 20px hsl(108, 100%, 50%), 36px 21px hsl(113.4, 100%, 50%), 38px 22px hsl(118.8, 100%, 50%), 39px 23px hsl(124.2, 100%, 50%), 41px 24px hsl(129.6, 100%, 50%), 42px 25px hsl(135, 100%, 50%), 43px 26px hsl(140.4, 100%, 50%), 45px 27px hsl(145.8, 100%, 50%), 46px 28px hsl(151.2, 100%, 50%), 47px 29px hsl(156.6, 100%, 50%), 48px 30px hsl(162, 100%, 50%), 49px 31px hsl(167.4, 100%, 50%), 50px 32px hsl(172.8, 100%, 50%), 51px 33px hsl(178.2, 100%, 50%), 52px 34px hsl(183.6, 100%, 50%), 53px 35px hsl(189, 100%, 50%), 54px 36px hsl(194.4, 100%, 50%), 55px 37px hsl(199.8, 100%, 50%), 55px 38px hsl(205.2, 100%, 50%), 56px 39px hsl(210.6, 100%, 50%), 57px 40px hsl(216, 100%, 50%), 57px 41px hsl(221.4, 100%, 50%), 58px 42px hsl(226.8, 100%, 50%), 58px 43px hsl(232.2, 100%, 50%), 58px 44px hsl(237.6, 100%, 50%); font-size: 25px;";
+
+console.log('%cThis%s', css, ' is Terry. What up. Just so you know, I am in the early stages of ripping this script to pieces. So I\'ll be dropping crap to the console LEFT AND RIGHT. If you don\'t like it, go %&!$ a *%#&!');
 
 
 
 $.fn.binder = function (type, schema, object) {
 	var self = this;
+
+
+
+
+
+
+
+
+
+	/****************************************************************************************
+    * Initialization Check!
+    * -> The following are some initial checks that occur when first initializing terrybinder!
+    ******************************************************************************************/
 
 
 	/* if a jQuery element and a model reference aren't included, check the page and bind all referenced models */
@@ -57,6 +72,21 @@ $.fn.binder = function (type, schema, object) {
 	}
 
 
+
+
+
+
+
+
+
+
+
+
+	/****************************************************************************************
+	*
+	*   The Bind!
+	*
+	****************************************************************************************/
 
 	var scope = $.binder.scope[schema]; // set scope for the particular bound model.
 
@@ -142,18 +172,19 @@ function binder_create(scope, template) {
 	self.data('default', $.extend(true, {}, scope.model));
 	self.data('model', scope.model);
 
-	self.on('input paste cut', 'input[data-model],textarea[data-model], div[data-model][contenteditable="true"]', function (e) {
+	self.on('input', 'input[data-model],textarea[data-model], div[data-model][contenteditable="true"], select[data-model]', function (e) {
 		binder_internal_change.call(this, e, self, self.data('model'), scope.schema, scope);
 	});
 
-	self.on('change', 'input[data-model][type="radio"],input[data-model][type="checkbox"],select[data-model]', function (e) {
+	self.on('input change', 'input[data-model][type="radio"],input[data-model][type="checkbox"],input[type="hidden"]', function (e) {
 		binder_internal_change.call(this, e, self, self.data('model'), scope.schema, scope);
 	});
-
+/*
 	self.on('DOMSubtreeModified', '[data-model-calc]', function (e) {
 		binder_internal_change.call(this, e, self, self.data('model'), scope.schema, scope);
+        console.warn(123);
 	});
-
+*/
 
 
 	binder_refresh.call(self, schema);
@@ -710,7 +741,7 @@ function	binder_show(self, model, name, schema, scope) { // deal with conditiona
 function binder_delay(fn) {
 	setTimeout(function() {
 		fn();
-	}, 12);
+	}, 2);
 }
 
 function binder_build_model(schema){
